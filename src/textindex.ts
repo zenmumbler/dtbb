236c1ebd889620db6dfabd16b954a0f2f7518b93
Arthur Langereis <puurtuur@me.com>
2016-04-25 19:33:45 +0200

    remove diacritics from data and queries

--- src/textindex.ts
+++ src/textindex.ts
@@ -1,9 +1,88 @@
-// textindex.ts - english script - part of DTBB (https://github.com/zenmumbler/dtbb)
+// textindex.ts - part of DTBB (https://github.com/zenmumbler/dtbb)
 // (c) 2016 by Arthur Langereis (@zenmumbler)
+
+// Roman English script only
 
 import { unionSet } from "util";
 
 export type SerializedTextIndex = { [key: string]: number[] };
+
+const DiacriticCharMapping: { [ch: string]: string } = {
+	"À": "A", // LATIN CAPITAL LETTER A WITH GRAVE
+	"Á": "A", // LATIN CAPITAL LETTER A WITH ACUTE
+	"Â": "A", // LATIN CAPITAL LETTER A WITH CIRCUMFLEX
+	"Ã": "A", // LATIN CAPITAL LETTER A WITH TILDE
+	"Ä": "A", // LATIN CAPITAL LETTER A WITH DIAERESIS
+	"Å": "A", // LATIN CAPITAL LETTER A WITH RING ABOVE
+
+	"Ç": "C", // LATIN CAPITAL LETTER C WITH CEDILLA
+
+	"È": "E", // LATIN CAPITAL LETTER E WITH GRAVE
+	"É": "E", // LATIN CAPITAL LETTER E WITH ACUTE
+	"Ê": "E", // LATIN CAPITAL LETTER E WITH CIRCUMFLEX
+	"Ë": "E", // LATIN CAPITAL LETTER E WITH DIAERESIS
+
+	"Ì": "I", // LATIN CAPITAL LETTER I WITH GRAVE
+	"Í": "I", // LATIN CAPITAL LETTER I WITH ACUTE
+	"Î": "I", // LATIN CAPITAL LETTER I WITH CIRCUMFLEX
+	"Ï": "I", // LATIN CAPITAL LETTER I WITH DIAERESIS
+
+	"Ñ": "N", // LATIN CAPITAL LETTER N WITH TILDE
+
+	"Ò": "O", // LATIN CAPITAL LETTER O WITH GRAVE
+	"Ó": "O", // LATIN CAPITAL LETTER O WITH ACUTE
+	"Ô": "O", // LATIN CAPITAL LETTER O WITH CIRCUMFLEX
+	"Õ": "O", // LATIN CAPITAL LETTER O WITH TILDE
+	"Ö": "O", // LATIN CAPITAL LETTER O WITH DIAERESIS
+	"Ø": "O", // LATIN CAPITAL LETTER O WITH STROKE
+
+	"Ù": "U", // LATIN CAPITAL LETTER U WITH GRAVE
+	"Ú": "U", // LATIN CAPITAL LETTER U WITH ACUTE
+	"Û": "U", // LATIN CAPITAL LETTER U WITH CIRCUMFLEX
+	"Ü": "U", // LATIN CAPITAL LETTER U WITH DIAERESIS
+
+	"Ý": "Y", // LATIN CAPITAL LETTER Y WITH ACUTE
+
+	"ß": "ss", // LATIN SMALL LETTER SHARP S
+
+	"à": "a", // LATIN SMALL LETTER A WITH GRAVE
+	"á": "a", // LATIN SMALL LETTER A WITH ACUTE
+	"â": "a", // LATIN SMALL LETTER A WITH CIRCUMFLEX
+	"ã": "a", // LATIN SMALL LETTER A WITH TILDE
+	"ä": "a", // LATIN SMALL LETTER A WITH DIAERESIS
+	"å": "a", // LATIN SMALL LETTER A WITH RING ABOVE
+
+	"ç": "c", // LATIN SMALL LETTER C WITH CEDILLA
+	"è": "e", // LATIN SMALL LETTER E WITH GRAVE
+	"é": "e", // LATIN SMALL LETTER E WITH ACUTE
+	"ê": "e", // LATIN SMALL LETTER E WITH CIRCUMFLEX
+	"ë": "e", // LATIN SMALL LETTER E WITH DIAERESIS
+	"ì": "i", // LATIN SMALL LETTER I WITH GRAVE
+	"í": "i", // LATIN SMALL LETTER I WITH ACUTE
+	"î": "i", // LATIN SMALL LETTER I WITH CIRCUMFLEX
+	"ï": "i", // LATIN SMALL LETTER I WITH DIAERESIS
+
+	"ñ": "n", // LATIN SMALL LETTER N WITH TILDE
+	"ò": "o", // LATIN SMALL LETTER O WITH GRAVE
+	"ó": "o", // LATIN SMALL LETTER O WITH ACUTE
+	"ô": "o", // LATIN SMALL LETTER O WITH CIRCUMFLEX
+	"õ": "o", // LATIN SMALL LETTER O WITH TILDE
+	"ö": "o", // LATIN SMALL LETTER O WITH DIAERESIS
+	"ø": "o", // LATIN SMALL LETTER O WITH STROKE
+
+	"ù": "u", // LATIN SMALL LETTER U WITH GRAVE
+	"ú": "u", // LATIN SMALL LETTER U WITH ACUTE
+	"û": "u", // LATIN SMALL LETTER U WITH CIRCUMFLEX
+	"ü": "u", // LATIN SMALL LETTER U WITH DIAERESIS
+
+	"ý": "y", // LATIN SMALL LETTER Y WITH ACUTE
+	"ÿ": "y", // LATIN SMALL LETTER Y WITH DIAERESIS
+}
+
+const DiacriticsMatcher = /[ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåçèéêëìíîïñòóôõöøùúûüýÿ]/;
+const DiacriticCharMatchers: { [c: string]: RegExp } = {};
+Object.keys(DiacriticCharMapping).forEach(c => DiacriticCharMatchers[c] = new RegExp(c, "g"));
+
 
 export class TextIndex {
 	private data_ = new Map<string, Set<number>>();
@@ -65,6 +144,16 @@ export class TextIndex {
 		}
 	}
 
+	stripDiacritics(term: string) {
+		var r: RegExpMatchArray;
+		// if a mapped character appears anywhere in the term, replace all occurances at once
+		while (r = term.match(DiacriticsMatcher)) {
+			var mc = term[r.index];
+			term = term.replace(DiacriticCharMatchers[mc], DiacriticCharMapping[mc]);
+		}
+		return term;
+	}
+
 	tokenizeString(s: string) {
 		var cs = s.toLowerCase().replace(/['-]/g, "").replace(/[^a-z0-9]/g, " ").replace(/ +/g, " ").trim();
 		var tokens = cs.split(" ");
@@ -75,6 +164,7 @@ export class TextIndex {
 		var boxedRef = [ref];
 		var tokenSet = this.tokenizeString(rs);
 		tokenSet.forEach(token => {
+			token = this.stripDiacritics(token);
 			var ngrams = this.wordNGrams(token);
 			ngrams.forEach(ngram => {
 				if (! this.data_.has(ngram)) {
@@ -98,6 +188,8 @@ export class TextIndex {
 			if (term.length > this.MAX_NGRAM_LENGTH) {
 				term = term.substr(0, this.MAX_NGRAM_LENGTH);
 			}
+
+			term = this.stripDiacritics(term);
 			if (this.data_.has(term)) {
 				termIndexSets.push(this.data_.get(term));
 			}
