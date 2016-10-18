9a8133ad4dd4d30670221aff8c6cf9c496af6e2d
Arthur Langereis <puurtuur@me.com>
2016-10-18 19:12:09 +0200

    add util func to replace Set(iterable) constructor

--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,7 @@
 
 export function loadTypedJSON<T>(url: string): Promise<T> {
 	return new Promise<T>((resolve, reject) => {
-		var xhr = new XMLHttpRequest();
+		const xhr = new XMLHttpRequest();
 		xhr.open("GET", url);
 		xhr.overrideMimeType("application/json");
 		xhr.responseType = "json";
@@ -27,7 +27,7 @@ export function elemList<T extends HTMLElement>(sel: string, base: NodeSelector 
 
 
 export function intersectSet(dest: Set<number>, other: Set<number>) {
-	var union = new Set<number>();
+	const union = new Set<number>();
 	dest.forEach(index => {
 		if (other.has(index)) {
 			union.add(index);
@@ -36,3 +36,20 @@ export function intersectSet(dest: Set<number>, other: Set<number>) {
 	return union;
 }
 
+
+export function newSetFromArray<T>(source: T[]) {
+	// one would think the instructor from an iterable is faster
+	// than just looping over it and doing 100s of 1000s of function calls
+	// on Safari and Chrome, one would be wrong.
+
+	// this func does the equivalent of:
+	// new Set<T>(source);
+
+	const set = new Set<T>();
+	const len = source.length;
+	for (let vi = 0; vi < len; ++vi) {
+		set.add(source[vi]);
+	}
+
+	return set;
+}
