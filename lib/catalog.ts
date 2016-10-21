d695a52ecedfa1aaa846cd2c4457ec23194aafdc
Arthur Langereis <puurtuur@me.com>
2016-10-21 17:38:57 +0200

    make entry and catalog type more explicit with interfaces n stuff

--- lib/catalog.ts
+++ lib/catalog.ts
@@ -6,7 +6,11 @@ export interface EntryListing {
 	thumbs: string[];
 }
 
-export const enum Platform {
+// ----
+
+export type Platform = "desktop" | "win" | "mac" | "linux" | "web" | "java" | "vr" | "mobile";
+
+export const enum PlatformMask {
 	None = 0,
 	Desktop = 1,
 	Win = 2,
@@ -19,16 +23,58 @@ export const enum Platform {
 }
 
 export const PlatformList = (() => {
-	const platforms: Platform[] = [];
-	let platMask = Platform.Desktop;
-	while (platMask <= Platform.Mobile) {
+	const platforms: PlatformMask[] = [];
+	let platMask = PlatformMask.Desktop;
+	while (platMask <= PlatformMask.Mobile) {
 		platforms.push(platMask);
 		platMask <<= 1;
 	}
 	return Object.freeze(platforms);
 })();
 
-export type Category = "" | "compo" | "jam";
+export function nameListForPlatformMask(mask: PlatformMask): Platform[] {
+	const names: Platform[] = [];
+
+	if (mask & PlatformMask.Desktop) { names.push("desktop"); }
+	if (mask & PlatformMask.Win) { names.push("win"); }
+	if (mask & PlatformMask.Mac) { names.push("mac"); }
+	if (mask & PlatformMask.Linux) { names.push("linux"); }
+	if (mask & PlatformMask.Web) { names.push("web"); }
+	if (mask & PlatformMask.Java) { names.push("java"); }
+	if (mask & PlatformMask.VR) { names.push("vr"); }
+	if (mask & PlatformMask.Mobile) { names.push("mobile"); }
+
+	return names;
+}
+
+// ----
+
+export type Category = "compo" | "jam";
+
+export type RatingArea = "audio" | "community" | "coolness" | "fun" | "graphics" | "humor" | "innovation" | "overall" | "theme";
+
+export interface EntryRating {
+	area: RatingArea;
+	rank: number;
+	score: number;
+}
+
+export interface LabelledLink {
+	label: string;
+	url: string;
+}
+
+export interface Screenshot {
+	thumbnail_url: string;
+	full_url: string;
+}
+
+export interface Author {
+	name: string;
+	uid: number;
+	avatar_url: string;
+	home_url: string;
+}
 
 export interface Entry {
 	ld_issue: number;
@@ -39,24 +85,25 @@ export interface Entry {
 	thumbnail_url: string;
 	entry_url: string;
 
-	author: {
-		name: string;
-		uid: number;
-		avatar_url: string;
-		home_url: string;
-	};
-
-	screens: {
-		thumbnail_url: string;
-		full_url: string;
-	}[];
-
-	links: {
-		title: string;
-		url: string;
-	}[];
-
-	platform: Platform;
+	author: Author;
+	screens: Screenshot[];
+	links: LabelledLink[];
+	ratings: EntryRating[];
+	platforms: Platform[];
 }
 
-export type Catalog = Entry[];
+// ----
+
+export interface IssueStats {
+	entries: number;
+	compoEntries: number;
+	jamEntries: number;
+	ratingDistribution: { [area: string]: number; };
+}
+
+// ----
+
+export interface Catalog {
+	stats: IssueStats;
+	entries: Entry[];
+}
