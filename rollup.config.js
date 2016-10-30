2418ccdb9f7ccb1b2da346959864a2f0d5b15181
Arthur Langereis <puurtuur@me.com>
2016-10-30 21:29:33 +0100

    back to rollup using TS es5 output with es6 modules, works like a charm and no bubling

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,4 @@
-import buble from "rollup-plugin-buble";
 import uglify from "rollup-plugin-uglify";
-
-// buble currently has a bug with for of loops
-function fixBuble (options) {
-	return {
-		name: 'fixBuble',
-		transform: function (code, id) {
-			return code.replace(/= void 0 (of|in)/g, (_, ofIn) => ofIn);
-		}
-	};
-}
 
 export default {
 	entry: "js/src/app.js",
@@ -17,12 +6,6 @@ export default {
 	format: "iife",
 	moduleName: "dtbb",
 	plugins: [
-		buble({
-			transforms: {
-				forOf: false
-			}
-		}),
-		fixBuble(),
 		// uglify()
 	]
 };
