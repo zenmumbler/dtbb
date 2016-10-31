6acc8c8bcefedb89282a77de7059c615e5d130ac
Arthur Langereis <puurtuur@me.com>
2016-10-31 19:16:26 +0100

    binding a watchable to arbitrary html elements + impl in state for filter controls

--- src/filtercontrols.ts
+++ src/filtercontrols.ts
@@ -1,48 +1,25 @@
 // filtercontrols.ts - part of DTBB (https://github.com/zenmumbler/dtbb)
 // (c) 2016 by Arthur Langereis (@zenmumbler)
 
-import { Category } from "../lib/catalog";
-// import { arrayFromSet } from "../lib/setutil";
-import { elem, elemList } from "./domutil";
+import { watchableBinding } from "./watchablebinding";
 import { GamesBrowserState } from "./state";
 
 export class FilterControls {
 	constructor(containerElem_: HTMLElement, state_: GamesBrowserState) {
-		// issue selector
-		const issueSelect = elem<HTMLSelectElement>("select[data-filter=issue]", containerElem_);
-		issueSelect.onchange = _ => {
-			state_.setIssue(parseInt(issueSelect.value));
-		};
-		state_.issue.watch(newIssue => {
-			const curIssue = parseInt(issueSelect.value);
-			if (curIssue !== newIssue) {
-				issueSelect.value = String(newIssue);
-			}
-		});
+		// LD issue selector
+		watchableBinding(state_.issue, "select[data-filter=issue]", containerElem_)
+			.broadcast(issue => { state_.setIssue(issue); });
 
 		// category radios
-		const categoryControls = elemList<HTMLInputElement>("input[name=category]", containerElem_);
-		for (const cc of categoryControls) {
-			cc.onchange = evt => {
-				const ctrl = <HTMLInputElement>evt.target;
-				if (ctrl.checked) {
-					state_.setCategory(<Category>ctrl.value);
-				}
-			};
-		}
+		watchableBinding(state_.category, "input[name=category]", containerElem_)
+			.broadcast(category => { state_.setCategory(category); });
 
 		// platform selector
-		const platformSelect = elem<HTMLSelectElement>("select[data-filter=platform]", containerElem_);
-		platformSelect.onchange = _ => {
-			state_.setPlatform(parseInt(platformSelect.value));
-		};
+		watchableBinding(state_.platform, "select[data-filter=platform]", containerElem_)
+			.broadcast(platform => { state_.setPlatform(platform); });
 
-		// full text search
-		const searchControl = elem<HTMLInputElement>("#terms", containerElem_);
-		searchControl.oninput = _ => {
-			state_.setQuery(searchControl.value);
-		};
-
-		searchControl.focus();
+		// query terms
+		watchableBinding(state_.query, "#terms", containerElem_)
+			.broadcast(query => { state_.setQuery(query); });
 	}
 }
---
+++ src/watchablebinding.ts
@@ -0,0 +1,144 @@
+// watchablebinding.ts - part of DTBB (https://github.com/zenmumbler/dtbb)
+// (c) 2016 by Arthur Langereis (@zenmumbler)
+
+import { Watchable } from "../lib/watchable";
+
+export class WatchableInputBinding<T extends (string | number | boolean)> {
+	private broadcastFn_?: (v: T) => void;
+	private acceptFn_?: (v: T) => void;
+
+	constructor(private watchable_: Watchable<T>, private elems_: HTMLElement[]) {
+		for (const elem of elems_) {
+			this.bindElement(elem);
+		}
+
+		watchable_.watch(newVal => {
+			this.acceptChange(newVal);
+		});
+	}
+
+	broadcast(fn: (v: T) => void) {
+		this.broadcastFn_ = fn;
+		return this;
+	}
+
+	accept(fn: (v: T) => void) {
+		this.acceptFn_ = fn;
+		return this;
+	}
+
+	private broadcastChange(newValue: T) {
+		if (this.broadcastFn_) {
+			this.broadcastFn_(newValue);
+		}
+	}
+
+	private acceptChange(newValue: T) {
+		if (this.acceptFn_) {
+			this.acceptFn_(newValue);
+		}
+		else {
+			const watchableValue = String(newValue);
+
+			for (const elem of this.elems_) {
+				const currentValue = this.getElementValue(elem);
+				if (watchableValue !== currentValue) {
+					this.setElementValue(elem, newValue);
+				}
+			}
+		}
+	}
+
+	private getElementValue(elem: HTMLElement): string | undefined {
+		const tag = elem.nodeName.toLowerCase();
+		switch (tag) {
+			case "select":
+			case "textarea":
+				return (elem as (HTMLSelectElement & HTMLTextAreaElement)).value;
+
+			case "input": {
+				const type = (elem as HTMLInputElement).type;
+				if (type === "radio" || type === "checkbox") {
+					return (elem as HTMLInputElement).checked ? (elem as HTMLInputElement).value : undefined;
+				}
+				return (elem as HTMLInputElement).value;
+			}
+			default:
+				return elem.textContent || "";
+		}
+	}
+
+	private setElementValue(elem: HTMLElement, newValue: T) {
+		const tag = elem.nodeName.toLowerCase();
+		switch (tag) {
+			case "select":
+			case "textarea":
+				(elem as HTMLSelectElement & HTMLTextAreaElement).value = String(newValue);
+				break;
+
+			case "input": {
+				const type = (elem as HTMLInputElement).type;
+				if (type === "radio" || type === "checkbox") {
+					(elem as HTMLInputElement).checked = (newValue == (elem as HTMLInputElement).value);
+				}
+				else {
+					(elem as HTMLInputElement).value = String(newValue);
+				}
+				break;
+			}
+			default:
+				elem.textContent = String(newValue);
+				break;
+		}
+	}
+
+	private bindElement(elem: HTMLElement) {
+		const tag = elem.nodeName.toLowerCase();
+		const type = (elem as HTMLInputElement).type;
+		let eventName: string;
+		if (tag === "input" && (type === "radio" || type === "checkbox")) {
+			eventName = "change";
+		}
+		else {
+			eventName = "input";
+		}
+
+		elem.addEventListener(eventName, _ => {
+			const valueStr = this.getElementValue(elem);
+			if (valueStr === undefined) {
+				return;
+			}
+
+			const watchableType = typeof this.watchable_.get();
+			if (watchableType === "number") {
+				let value: number;
+				value = parseInt(valueStr);
+				this.broadcastChange(value as T);
+			}
+			else if (watchableType === "boolean") {
+				let value: boolean;
+				value = (valueStr === "true");
+				this.broadcastChange(value as T);
+			}
+			else if (watchableType === "string") {
+				let value: string;
+				value = valueStr;
+				this.broadcastChange(value as T);
+			}
+			else {
+				console.warn(`Don't know what to do with a watchable of type ${watchableType}`);
+			}
+		});
+	}
+}
+
+export function watchableBinding<T extends (number | string | boolean)>(w: Watchable<T>, element: HTMLElement | HTMLElement[]): WatchableInputBinding<T>;
+export function watchableBinding<T extends (number | string | boolean)>(w: Watchable<T>, selector: string, context?: NodeSelector): WatchableInputBinding<T>;
+export function watchableBinding<T extends (number | string | boolean)>(w: Watchable<T>, elemOrSel: string | HTMLElement | HTMLElement[], context?: NodeSelector) {
+	const elems = (
+		(typeof elemOrSel === "string")
+		? ([].slice.call((context || document).querySelectorAll(elemOrSel)) as HTMLElement[])
+		: (Array.isArray(elemOrSel) ? elemOrSel : [elemOrSel])
+	);
+	return new WatchableInputBinding<T>(w, elems);
+}
