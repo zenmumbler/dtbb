3005f4a5a0291cdecd6bb922bc0e5f4540351094
Arthur Langereis <puurtuur@me.com>
2016-10-31 16:00:13 +0100

    adopt watchable in filter controls

--- src/filtercontrols.ts
+++ src/filtercontrols.ts
@@ -1,18 +1,24 @@
 // filtercontrols.ts - part of DTBB (https://github.com/zenmumbler/dtbb)
 // (c) 2016 by Arthur Langereis (@zenmumbler)
 
-import { Platforms, Category } from "../lib/catalog";
+import { Category } from "../lib/catalog";
 // import { arrayFromSet } from "../lib/setutil";
 import { elem, elemList } from "./domutil";
 import { GamesBrowserState } from "./state";
 
 export class FilterControls {
 	constructor(containerElem_: HTMLElement, state_: GamesBrowserState) {
-		// full text search
-		const searchControl = elem<HTMLInputElement>("#terms", containerElem_);
-		searchControl.oninput = _ => {
-			state_.query = searchControl.value;
+		// issue selector
+		const issueSelect = elem<HTMLSelectElement>("select[data-filter=issue]", containerElem_);
+		issueSelect.onchange = _ => {
+			state_.setIssue(parseInt(issueSelect.value));
 		};
+		state_.issue.watch(newIssue => {
+			const curIssue = parseInt(issueSelect.value);
+			if (curIssue !== newIssue) {
+				issueSelect.value = String(newIssue);
+			}
+		});
 
 		// category radios
 		const categoryControls = elemList<HTMLInputElement>("input[name=category]", containerElem_);
@@ -20,24 +26,22 @@ export class FilterControls {
 			cc.onchange = evt => {
 				const ctrl = <HTMLInputElement>evt.target;
 				if (ctrl.checked) {
-					state_.category = <Category>ctrl.value;
+					state_.setCategory(<Category>ctrl.value);
 				}
 			};
 		}
 
 		// platform selector
-		const platformSelect = elem<HTMLSelectElement>("select", containerElem_);
-		platformSelect.onchange = evt => {
-			const ctrl = <HTMLSelectElement>evt.target;
-			state_.platform = parseInt(ctrl.value);
+		const platformSelect = elem<HTMLSelectElement>("select[data-filter=platform]", containerElem_);
+		platformSelect.onchange = _ => {
+			state_.setPlatform(parseInt(platformSelect.value));
 		};
-		for (const platKey in Platforms) {
-			const plat = Platforms[platKey];
-			const pe = document.createElement("option");
-			pe.value = String(plat.mask);
-			pe.textContent = plat.label;
-			
-		}
+
+		// full text search
+		const searchControl = elem<HTMLInputElement>("#terms", containerElem_);
+		searchControl.oninput = _ => {
+			state_.setQuery(searchControl.value);
+		};
 
 		searchControl.focus();
 	}
