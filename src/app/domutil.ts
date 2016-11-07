088e7db6b50c237ac6187fff319a03736fb67012
Arthur Langereis <puurtuur@me.com>
2016-11-07 16:54:59 +0100

    remove loadTypedJSON from domutil

--- src/app/domutil.ts
+++ src/app/domutil.ts
@@ -1,19 +1,5 @@
-
-
-export function loadTypedJSON<T>(url: string): Promise<T> {
-	return new Promise<T>((resolve, reject) => {
-		const xhr = new XMLHttpRequest();
-		xhr.open("GET", url);
-		xhr.overrideMimeType("application/json");
-		xhr.responseType = "json";
-		xhr.onload = function() {
-			resolve(<T>xhr.response);
-		};
-		xhr.onerror = reject;
-		xhr.send(null);
-	});
-}
-
+// domutil.ts - part of DTBB (https://github.com/zenmumbler/dtbb)
+// (c) 2016 by Arthur Langereis (@zenmumbler)
 
 export function elem<T extends HTMLElement>(sel: string, base: NodeSelector = document) {
 	return <T>(base.querySelector(sel));
