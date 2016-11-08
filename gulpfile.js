ad8ee36e279ddc61decd777c1bbdbca3f1421a06
Arthur Langereis <puurtuur@me.com>
2016-11-08 20:14:44 +0100

    gulpfile with tasks to (auto-)roll the 3 targets

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+// gulpfile.js - part of DTBB (https://github.com/zenmumbler/dtbb)
+// (c) 2016 by Arthur Langereis (@zenmumbler)
+
+const gulp = require("gulp");
+const rollup = require("rollup-stream");
+const source = require("vinyl-source-stream");
+const uglify = require("gulp-uglify");
+
+// bundle main site code
+gulp.task("rollsite", function() {
+	return rollup({
+		entry: "site/build/app/app/app.js",
+		format: "iife",
+		moduleName: "dtbb"
+	})
+	.pipe(source("dtbb.js"))
+	.pipe(uglify({
+		mangle: false,
+		compress: true
+	}))
+	.pipe(gulp.dest("site"));
+});
+
+// bundle site worker
+gulp.task("rollworker", function() {
+	return rollup({
+		entry: "site/build/workers/workers/task_indexer.js",
+		format: "iife",
+	})
+	.pipe(source("task_indexer.js"))
+	.pipe(uglify({
+		mangle: false,
+		compress: true
+	}))
+	.pipe(gulp.dest("site"));
+});
+
+// bundle import node app
+gulp.task("rollimport", function() {
+	return rollup({
+		entry: "import/build/import/import.js",
+		format: "cjs",
+	})
+	.pipe(source("import.js"))
+	.pipe(gulp.dest("import"));
+});
+
+// roll all 3 by default
+gulp.task("default", ["rollsite", "rollworker", "rollimport"], () => {});
+
+// auto-roller
+gulp.task("watchroll", function() {
+	gulp.watch("site/build/**/*.js", ["rollsite", "rollworker"]);
+	gulp.watch("import/build/**/*.js", ["rollimport"]);
+});
