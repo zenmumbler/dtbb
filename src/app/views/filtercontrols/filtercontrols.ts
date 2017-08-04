5dd2915fc3d97f08e21df70304bda75bbe0a5cfa
Arthur Langereis <puurtuur@me.com>
2017-08-04 18:49:44 +0200

    only LD38 does not have platform support

--- src/app/views/filtercontrols/filtercontrols.ts
+++ src/app/views/filtercontrols/filtercontrols.ts
@@ -11,7 +11,7 @@ export class FilterControls {
 		watchableBinding(state_.issue, "select[data-filter=issue]", containerElem_)
 			.broadcast(issue => {
 				state_.setIssue(issue);
-				if (issue > 37) {
+				if (issue === 38) {
 					state_.setPlatform(0);
 				}
 				elem<HTMLSelectElement>("select[data-filter=platform]").disabled = issue > 37;
@@ -32,7 +32,7 @@ export class FilterControls {
 		state_.loading.watch(loading => {
 			if (! loading) {
 				elem<HTMLInputElement>("#terms").focus();
-				elem<HTMLSelectElement>("select[data-filter=platform]").disabled = state_.issue.get() > 37;
+				elem<HTMLSelectElement>("select[data-filter=platform]").disabled = state_.issue.get() === 38;
 			}
 		});
 	}
