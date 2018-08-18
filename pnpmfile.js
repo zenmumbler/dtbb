fbe63fec142b07e2d3ed6ac779bcaeed55f9062c
Arthur Langereis <puurtuur@me.com>
2018-08-18 18:26:24 +0200

    chore: convert to pnpm

---
+++ pnpmfile.js
@@ -0,0 +1,18 @@
+const ROLLUP_VERSION = "^0.64.1";
+
+function readPackage(pkg, context) {
+	// Override the manifest of foo@1 after downloading it from the registry
+	// Replace all dependencies with bar@2
+	if (pkg.name === "rollup-stream") {
+		pkg.dependencies.rollup = ROLLUP_VERSION;
+		context.log(`Forced rollup of rollup-stream to be ${ROLLUP_VERSION}`);
+	}
+	
+	return pkg;
+}
+
+module.exports = {
+	hooks: {
+		readPackage
+	}
+};
