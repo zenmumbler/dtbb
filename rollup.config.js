b89f1462ab4dca114728e08375a43a57bf96eccd
Arthur Langereis <puurtuur@me.com>
2020-08-12 16:37:19 +0200

    chore: add svelte index and build commands

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,34 @@
+// @ts-check
+import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
-import common from "@rollup/plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
+
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+	
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
 
 export default [
 	// import
@@ -11,7 +40,7 @@ export default [
 		}],
 		plugins: [
 			resolve(),
-			common(),
+			commonjs(),
 		],
 		external: ["fs", "mkdirp", "got", "jsdom"]
 	},
@@ -25,7 +54,7 @@ export default [
 		}],
 		plugins: [
 			resolve(),
-			common(),
+			commonjs(),
 		]
 	},
 
@@ -39,7 +68,57 @@ export default [
 		}],
 		plugins: [
 			resolve(),
-			common(),
+			commonjs(),
 		]
+	},
+
+	// svelte app
+	{
+		input: "src/svapp/main.ts",
+		output: {
+			sourcemap: true,
+			format: "iife",
+			name: "dtbb",
+			file: "site/svdtbb.js"
+		},
+		plugins: [
+			svelte({
+				// enable run-time checks when not in production
+				dev: !production,
+				// we"ll extract any component CSS out into
+				// a separate file - better for performance
+				css: css => {
+					css.write("site/svdtbb.css", false);
+				},
+				preprocess: sveltePreprocess(),
+			}),
+	
+			// If you have external dependencies installed from
+			// npm, you"ll most likely need these plugins. In
+			// some cases you"ll need additional configuration -
+			// consult the documentation for details:
+			// https://github.com/rollup/plugins/tree/master/packages/commonjs
+			resolve({
+				browser: true,
+				dedupe: ["svelte"]
+			}),
+			commonjs(),
+			typescript({ sourceMap: !production, tsconfig: "src/svapp/tsconfig.json" }),
+	
+			// In dev mode, call `npm run start` once
+			// the bundle has been generated
+			!production && serve(),
+	
+			// Watch the `public` directory and refresh the
+			// browser on changes when not in production
+			!production && livereload("site"),
+	
+			// If we"re building for production (npm run build
+			// instead of npm run dev), minify
+			production && terser()
+		],
+		watch: {
+			clearScreen: false
+		}
 	}
 ];
