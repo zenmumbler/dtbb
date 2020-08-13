5e3280493a7f5d325071f2cb9a503a60f4d607ea
Arthur Langereis <puurtuur@me.com>
2020-08-13 14:33:13 +0200

    feat: filter ui and basic stats

--- src/svapp/App.svelte
+++ src/svapp/App.svelte
@@ -1,30 +1,33 @@
 <script lang="ts">
-	export let name: string;
+	import { category, platform, query, issue } from "./stores";
+	import Controls from "./Controls.svelte";
+	import LoadingWall from "./LoadingWall.svelte";
+	import { filteredSet, loading, loadingRatio } from "./catalogstore";
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div id="dtbb" class="no-select">
+	<Controls />
+	{#if $loading}
+		<LoadingWall ratio={$loadingRatio} />
+	{/if}
+
+	<p>The current category is { $category }</p>
+	<p>The current platform is { $platform }</p>
+	<p>The current query is { $query }</p>
+	<p>The current issue is { $issue }</p>
+
+	<p>The current number of items filtered is { $filteredSet.size }</p>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+#dtbb {
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	min-width: 960px;
+	background-color: white;
+}
 </style>
