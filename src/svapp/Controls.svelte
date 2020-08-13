384ed07659ab08565c427b15152e2990d93fd6f1
Arthur Langereis <puurtuur@me.com>
2020-08-13 21:43:41 +0200

    ref: dynamic issue select generation

--- src/svapp/Controls.svelte
+++ src/svapp/Controls.svelte
@@ -1,81 +1,52 @@
 <script lang="ts">
-	import { Platforms } from "../lib/catalog";
+	import type { IssueInfo } from "../lib/catalog";
+	import { Platforms, IssueData } from "../lib/catalog";
 	import { platform, issue, query } from "./stores";
 	import CategoryFilter from "./CategoryFilter.svelte";
 
 	const platforms = Array.from(Object.values(Platforms));
+	
+	function groupByYear(a: IssueInfo[]) {
+		const years = new Map<number, IssueInfo[]>();
+		for (const issue of a) {
+			if (! years.has(issue.year)) {
+				years.set(issue.year, [issue]);
+			}
+			else {
+				years.get(issue.year)!.push(issue);
+			}
+		}
+		// reverse year sort with issues per year also sorted in reverse
+		return Array.from(years.entries())
+			.map(e => (e[1].sort((a, b) => b.issue - a.issue), e))
+			.sort((a, b) => b[0] - a[0]);
+	}
+
+	const issues = groupByYear(Array.from(Object.values(IssueData)));
 </script>
 
 <div id="controls">
 	<div class="filters">
 		<CategoryFilter />
 
+		<!-- svelte-ignore a11y-autofocus -->
 		<input type="text" bind:value={$query} id="terms" autofocus autocomplete="off" placeholder="Search Terms (2 char minimum)">
 
-		<select bind:value={$platform} data-filter="platform">
-			<option value="0">All Platforms</option>
+		<select bind:value={$platform} class="platform">
+			<option value={0}>All Platforms</option>
 			{#each platforms as { mask, label }}
 				<option value={mask}>{label}</option>
 			{/each}
 		</select>
 
-		<select bind:value={$issue} data-filter="issue">
-			<optgroup label="2020">
-				<option value={46}>LD 46 - Keep it alive</option>
+		<select bind:value={$issue} class="issue">
+			{#each issues as pair}
+			<optgroup label={""+pair[0]}>
+				{#each pair[1] as issin}
+					<option value={issin.issue}>LD {issin.issue} - {issin.theme}</option>	
+				{/each}
 			</optgroup>
-			<optgroup label="2019">
-				<option value={45}>LD 45 - Start with nothing</option>
-				<option value={44}>LD 44 - Your life is currency</option>
-			</optgroup>
-			<optgroup label="2018">
-				<option value={43}>LD 43 - Sacrifices must be made</option>
-				<option value={42}>LD 42 - Running out of Space</option>
-				<option value={41}>LD 41 - Two Incompatible Genres</option>
-			</optgroup>
-			<optgroup label="2017">
-				<option value={40}>LD 40 - The more you have, the worse it is</option>
-				<option value={39}>LD 39 - Running out of Power</option>
-				<option value={38}>LD 38 - A Small World</option>
-			</optgroup>
-			<optgroup label="2016">
-				<option value={37}>LD 37 - One Room</option>
-				<option value={36}>LD 36 - Ancient Technology</option>
-				<option value={35}>LD 35 - Shapeshift</option>
-			</optgroup>
-			<optgroup label="2015">
-				<option value={34}>LD 34 - Two Button Controls, Growing</option>
-				<option value={33}>LD 33 - You are the Monster</option>
-				<option value={32}>LD 32 - An Unconventional Weapon</option>
-			</optgroup>
-			<optgroup label="2014">
-				<option value={31}>LD 31 - Entire Game on One Screen</option>
-				<option value={30}>LD 30 - Connected Worlds</option>
-				<option value={29}>LD 29 - Beneath the Surface</option>
-			</optgroup>
-			<optgroup label="2013">
-				<option value={28}>LD 28 - You Only Get One</option>
-				<option value={27}>LD 27 - 10 Seconds</option>
-				<option value={26}>LD 26 - Minimalism</option>
-			</optgroup>
-			<optgroup label="2012">
-				<option value={25}>LD 25 - You are the Villain</option>
-				<option value={24}>LD 24 - Evolution</option>
-				<option value={23}>LD 23 - Tiny World</option>
-			</optgroup>
-			<optgroup label="2011">
-				<option value={22}>LD 22 - Alone</option>
-				<option value={21}>LD 21 - Escape</option>
-				<option value={20}>LD 20 - It’s Dangerous to go Alone! Take this!</option>
-			</optgroup>
-			<optgroup label="2010">
-				<option value={19}>LD 19 - Discovery</option>
-				<option value={18}>LD 18 - Enemies as Weapons</option>
-				<option value={17}>LD 17 - Islands</option>
-			</optgroup>
-			<optgroup label="2009">
-				<option value={16}>LD 16 - Exploration</option>
-				<option value={15}>LD 15 - Caverns</option>
-			</optgroup>
+			{/each}
 		</select>
 
 	</div>
@@ -113,11 +84,11 @@
 		min-width: 140px;
 		border-color: white;
 	}
-	select[data-filter=platform] {
+	select.platform {
 		margin-right: 16px;
 		width: 140px;
 	}
-	select[data-filter=issue] {
+	select.issue {
 		width: 220px;
 	}
 
@@ -126,10 +97,10 @@
 		input[type=text] {
 			width: 280px;
 		}
-		select[data-filter=platform] {
+		select.platform {
 			width: 200px;
 		}
-		select[data-filter=issue] {
+		select.issue {
 			width: 280px;
 		}
 	}
