66582a841bf0ae46054e5164c053068bc4365f47
zenmumbler <zen@stardazed.club>
2021-05-09 17:58:53 +0200

    chore: upgrade svelte stuff

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
+import css from "rollup-plugin-css-only";
 import tsc from "@rollup/plugin-typescript";
 import typescript from "typescript";
 
@@ -74,13 +75,12 @@ export default [
 			svelte({
 				// enable run-time checks when not in production
 				dev: !production,
-				// we"ll extract any component CSS out into
-				// a separate file - better for performance
-				css: css => {
-					css.write("dtbb.css", false);
-				},
 				preprocess: sveltePreprocess(),
 			}),
+
+			// we'll extract any component CSS out into
+			// a separate file - better for performance
+			css({ output: "dtbb.css" }),
 	
 			// If you have external dependencies installed from
 			// npm, you"ll most likely need these plugins. In
